2f4c2789bd76cdcf713cf4b659d0922c9a616135
ewfefw.wefew <ewfefw.wefew@mail.ru>
2019-10-01 07:05:16 +0600

    commit

---
+++ WebApplication3/WebApplication3/Controllers/BooksController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+
+        public ActionResult Index()
+        {
+            List<Books> authors;
+
+            using (Model1 db = new Model1())
+            {
+                authors = db.Books.ToList();
+
+            }
+            return View(authors);
+
+
+
+
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            Model1 db = new Model1();
+
+            SelectList books = new SelectList(db.Authors, "Id", "FirstName");
+            ViewBag.AuthorsList = books;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Books books)
+        {
+            using (Model1 db = new Model1())
+            {
+
+                db.Books.Add(books);
+                //try
+                //{
+                
+                    db.SaveChanges();
+
+                //}
+                //catch (DbEntityValidationException ex)
+                //{
+                //    foreach (DbEntityValidationResult validationError in ex.EntityValidationErrors)
+                //    {
+                //        Response.Write("Object: " + validationError.Entry.Entity.ToString());
+                //        Response.Write("");
+                //        foreach (DbValidationError err in validationError.ValidationErrors)
+                //        {
+                //            Response.Write(err.ErrorMessage + "");
+                //        }
+                //    }
+                //}
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            Books books;
+
+            using (Model1 db = new Model1())
+            {
+                books = db.Books.Where(a => a.Id == id).FirstOrDefault();
+            }
+            return View(books);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Books books)
+        {
+            using (Model1 db = new Model1())
+            {
+                var oldBook = db.Books.Where(a => a.Id == books.Id).FirstOrDefault();
+                oldBook.Title = books.Title;
+                oldBook.Price = books.Price;
+
+                db.SaveChanges();
+            }
+            return RedirectToActionPermanent("Index", "Books");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (Model1 db = new Model1())
+            {
+                var books = db.Books.Where(a => a.Id == id).FirstOrDefault();
+                db.Books.Remove(books);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index", "Books");
+            
+        }
+    }
+}
---
+++ WebApplication3/WebApplication3/Entities/Books.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3
+{
+    public partial class Books
+    {
+
+        public int Id { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public int AuthorId { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Title { get; set; }
+
+        public int? Pages { get; set; }
+        public int? Price { get; set; }
+
+        public virtual Authors Authors { get; set; }
+
+
+    }
+}
